6f2c98580d5d21a07818b689e72096570d0f2986
Breno Bering <bbering88@gmail.com>
2023-05-25 17:14:04 -0300

    Adicionada pasta thread e thread do carro preto

--- control/mainController.java
+++ control/mainController.java
@@ -7,12 +7,24 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import threads.threadBlackCar;
 
 public class mainController implements Initializable {
+    
+    // Imagens utilizadas no codigo
+
     @FXML
-    private ImageView blackCar;
+    private ImageView blackC;
+
+    // Threads
+    private threadBlackCar tBlackCar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.tBlackCar = new threadBlackCar();
+        tBlackCar.start();
+    }
+    
+    public void runStreet10(){
     }
 }
---
+++ threads/threadBlackCar.java
@@ -0,0 +1,19 @@
+package threads;
+
+import control.*;
+
+public class threadBlackCar extends Thread {
+  // Instanciando o controller
+  mainController controller = new mainController();
+
+  // Metodo que define o controller da thread
+  public void setController(mainController controller) {
+    this.controller = controller;
+  }
+
+  public void run() {
+    while (true) {
+      System.out.println("teste");
+    }
+  }
+}
